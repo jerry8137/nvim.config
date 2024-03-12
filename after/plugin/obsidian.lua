c3c7c8b035125fcd6acbd7fe9174ed42e3b40233
Jerry Hou <chunyu.hou@itri.org.tw>
2024-03-12 12:13:15 +0800

    obsidian template

--- after/plugin/obsidian.lua
+++ after/plugin/obsidian.lua
@@ -1,10 +1,28 @@
 vim.opt.conceallevel = 1
 require("obsidian").setup({
-      workspaces = {
-        {
-          name = "work",
-          path = "~/myObsidian",
-        },
-      },
-      -- see below for full list of options 👇
+  workspaces = {
+    {
+      name = "work",
+      path = "~/myObsidian",
+    },
+  },
+  -- see below for full list of options 👇
+  daily_notes = {
+    -- Optional, if you keep daily notes in a separate directory.
+    folder = "daily",
+    -- Optional, if you want to change the date format for the ID of daily notes.
+    date_format = "%Y-%m-%d",
+    -- Optional, if you want to change the date format of the default alias of daily notes.
+    alias_format = "%B %-d, %Y",
+    -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+    template = "daily.md",
+  },
+  templates = {
+    -- Optional, if you want to change the default template directory.
+    subdir = "templates",
+    date_format = "%Y-%m-%d-%a",
+    time_format = "%H:%M",
+  },
 })
+
+vim.keymap.set("n", "<leader>op", vim.cmd.ObsidianOpen);
