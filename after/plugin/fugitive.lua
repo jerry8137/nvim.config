e240770c810e50095bfcef1b3f2b6d898b78247e
Jerry Hou <chunyu.hou@itri.org.tw>
2024-05-31 09:42:51 +0800

    Show commit of current file

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,3 +1,5 @@
 vim.keymap.set("n", "<leader>gs", ":vertical Git<CR>");
 
 vim.keymap.set("n", "<leader>gl", ":Git log --oneline --decorate --graph --all<CR>")
+
+vim.keymap.set("n", "<leader>gc", ":0Gclog<CR>")
