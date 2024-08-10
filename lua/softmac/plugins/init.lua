c21152d393de17a0a8d611d29196e73071bf9151
Chun Yu Hou <jerry8137@gmail.com>
2024-08-10 13:04:39 +0800

    change theme

--- lua/softmac/plugins/init.lua
+++ lua/softmac/plugins/init.lua
@@ -6,6 +6,7 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
   },
   'navarasu/onedark.nvim',
+  'folke/tokyonight.nvim',
   'nvim-treesitter/nvim-treesitter',
   'nvim-treesitter/playground',
   'theprimeagen/harpoon',
