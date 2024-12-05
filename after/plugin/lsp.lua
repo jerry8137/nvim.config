ee71b2bc9d14a68d95f18b6663d405f9a7569692
Jerry Hou <chunyu.hou@itri.org.tw>
2024-12-05 10:54:53 +0800

    feat(lsp): setup pylsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,7 +3,7 @@ local lsp_zero = require('lsp-zero')
 lsp_zero.on_attach(function(client, bufnr)
   -- see :help lsp-zero-keybindings
   -- to learn the available actions
-  lsp_zero.default_keymaps({buffer = bufnr})
+  lsp_zero.default_keymaps({ buffer = bufnr })
 end)
 
 require('mason').setup({})
@@ -14,37 +14,69 @@ require('mason-lspconfig').setup({
   },
 })
 
-require'lspconfig'.lua_ls.setup {
+require 'lspconfig'.lua_ls.setup {
   settings = {
     Lua = {
       diagnostics = {
         -- Get the language server to recognize the `vim` global
-        globals = {'vim'},
+        globals = { 'vim' },
       },
     },
   },
 }
 local function get_clangd_cmd()
-    local clangd_wrapper_path = vim.fn.getcwd() .. '/.clangd-wrapper'
-    if vim.fn.filereadable(clangd_wrapper_path) == 1 then
-        return {clangd_wrapper_path}
-    else
-        -- Fallback clangd command if .clangd-wrapper is not found
-        return {'clangd'}
-    end
+  local clangd_wrapper_path = vim.fn.getcwd() .. '/.clangd-wrapper'
+  if vim.fn.filereadable(clangd_wrapper_path) == 1 then
+    return { clangd_wrapper_path }
+  else
+    -- Fallback clangd command if .clangd-wrapper is not found
+    return { 'clangd' }
+  end
 end
 
-require'lspconfig'.clangd.setup {
-    cmd = get_clangd_cmd(),
-    on_attach = function(client, bufnr)
-        -- Optional: add here your preferred key mappings, completion setup, etc.
-    end,
+require 'lspconfig'.clangd.setup {
+  cmd = get_clangd_cmd(),
+  on_attach = function(client, bufnr)
+    -- Optional: add here your preferred key mappings, completion setup, etc.
+  end,
 }
-require'lspconfig'.ruff.setup {
+
+local function get_ruff_cmd()
+  local ruff_wrapper_path = vim.fn.getcwd() .. '/.ruff-wrapper'
+  if vim.fn.filereadable(ruff_wrapper_path) == 1 then
+    return { ruff_wrapper_path }
+  else
+    -- Fallback clangd command if .clangd-wrapper is not found
+    return { 'pylsp' }
+  end
+end
+require 'lspconfig'.pylsp.setup {
+  cmd = get_ruff_cmd(),
   settings = {
-    args = {
-      -- Add your ruff args here
-      "--line-length=79",
-    },
+    pylsp = {
+      plugins = {
+        -- formatter options
+        black = { enabled = false },
+        autopep8 = { enabled = false },
+        yapf = {
+          enabled = true,
+          -- YAPF line length setting
+          args = { '--style', '{column_limit: 79}' }
+        },
+        -- linter options
+        pylint = { enabled = true, executable = "pylint",
+          -- Pylint line length setting
+          args = { '--max-line-length=100' } },
+        pyflakes = { enabled = false },
+        pycodestyle = { enabled = false },
+        -- type checker
+        pylsp_mypy = { enabled = true },
+        -- auto-completion options
+        jedi_completion = { fuzzy = true },
+        -- import sorting
+        pyls_isort = { enabled = true },
+
+      }
+    }
   }
 }
