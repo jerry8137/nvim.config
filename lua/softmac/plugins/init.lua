1b1cb65ed49ef8f71c87347e70b098dd25d891ad
Jerry Hou <chunyu.hou@itri.org.tw>
2025-02-20 15:08:53 +0800

    feat: use oil.nvim

--- lua/softmac/plugins/init.lua
+++ lua/softmac/plugins/init.lua
@@ -69,4 +69,17 @@ return {
       })
     end
   },
+  {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {
+      view_options = {
+        show_hidden = true,
+      },
+    },
+    -- Optional dependencies
+    dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    lazy = false,
+  },
 }
