76c3715dc2c6e6b74428a9f6d9ceaae3026fb5de
Jerry Hou <jerry8137@gmail.com>
2025-04-24 21:51:41 +0800

    feat: install leetcode.nvim

--- lua/softmac/plugins/init.lua
+++ lua/softmac/plugins/init.lua
@@ -86,4 +86,17 @@ return {
     dependencies = { { "echasnovski/mini.icons", opts = {} } },
     lazy = false,
   },
+  {
+    "kawre/leetcode.nvim",
+    build = ":TSUpdate html", -- if you have `nvim-treesitter` installed
+    dependencies = {
+        "nvim-telescope/telescope.nvim",
+        -- "ibhagwan/fzf-lua",
+        "nvim-lua/plenary.nvim",
+        "MunifTanjim/nui.nvim",
+    },
+    opts = {
+      lang="python";
+    },
+  }
 }
